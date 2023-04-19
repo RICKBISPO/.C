415f2dd13c55ff3cc4612c1819685cf3e9b6b6ed
Henrique Bispo <85528622+RICKBISPO@users.noreply.github.com>
2023-04-19 11:50:05 -0300

    Add files via upload

---
+++ produto_simples.c
@@ -0,0 +1,18 @@
+/* Produto de 2 números */
+
+#include <stdio.h>
+
+int main(){
+    
+    int PROD, n1, n2;
+
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+
+    PROD = n1 * n2;
+
+    printf("PROD = %d\n",PROD);
+
+
+    return 0;
+}
